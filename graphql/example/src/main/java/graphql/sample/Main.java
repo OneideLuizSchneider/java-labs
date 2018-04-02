19e15c4fcb3211e37740aa725521a9eff48dc020
OneideLuizSchneider <oneidels@gmail.com>
2018-04-01 22:58:53 -0300

    first commit

---
+++ graphql/example/src/main/java/graphql/sample/Main.java
@@ -0,0 +1,39 @@
+package graphql.sample;
+
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
+import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
+
+/**
+ *
+ * @author oneide
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        String schema = "type Query{first_sample: String}";
+
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+        RuntimeWiring runtimeWiring = newRuntimeWiring()
+                .type("Query", builder -> builder.dataFetcher("first_sample", new StaticDataFetcher("that's it :D")))
+                .build();
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+        GraphQL build = GraphQL.newGraphQL(graphQLSchema).build();
+        ExecutionResult executionResult = build.execute("{first_sample}");
+
+        System.out.println(executionResult.getData().toString());
+    }
+
+}
