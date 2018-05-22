6c1484be702ffcbba42a80b5cf89326fcd98e5d1
oneidels@gmail.com <oneidels@gmail.com>
2018-05-22 18:50:28 -0300

    add websocket

---
+++ websocket-server/websocket-server/src/main/java/br/com/websocketserver/Application.java
@@ -0,0 +1,30 @@
+package br.com.websocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ *
+ * @author oneide.schneider
+ */
+@EnableCaching
+@EnableAutoConfiguration
+@EnableScheduling
+@ComponentScan(basePackages = {"br.com.websocketserver"})
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
